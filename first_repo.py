09895ea585f69a2815f5abee588b6d762e10af01
Padmavel <126475154+Padmavel@users.noreply.github.com>
2023-02-27 18:42:32 +0530

    Update first_repo.py

--- first_repo.py
+++ first_repo.py
@@ -4,4 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Almonds & OatMeal')
 streamlit.text('Spinach soup')
-streamlit.text('Boiled Egg Whites'
+streamlit.text('Boiled Egg Whites')
