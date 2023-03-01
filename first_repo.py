d2a3c32831ce68eb974f9ff67b70229a3c19b68f
Padmavel <126475154+Padmavel@users.noreply.github.com>
2023-03-01 16:00:18 +0530

    Update first_repo.py

--- first_repo.py
+++ first_repo.py
@@ -41,3 +41,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#New section to display
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+
